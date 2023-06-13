91abc0aa6952cc2575cc84606a60e87a075700a5
Lauren Boyd <lboyd@esri.com>
2023-06-13 13:17:17 -0700

    feat: Add all charts to popup

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,6 @@ require([
 	const tractsLayer = new FeatureLayer({
 		url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/ACS_Educational_Attainment_Boundaries/FeatureServer/2",
 		outFields: ["*"],
-		fieldInfos: [
-			{
-				fieldName: "B15002_calc_numLTHSE",
-				label: "Adults(25+) Not High School Graduates",
-				format: {
-					digitSeparator: true,
-				},
-			},
-		],
 		popupTemplate: {
 			title: "{NAME} in {COUNTY}, {STATE}",
 			content: [
@@ -24,29 +15,196 @@ require([
 					type: "text",
 					text: `An estimated <b>{B15002_calc_numLTHSE}</b> adults age 25+ are not high school graduates, which is approximately <b>{B15002_calc_pctLTHSE}%</b> of all adults age 25+.`,
 				},
+				{
+					type: "media",
+					title: "Educational Attainment",
+					mediaInfos: [
+						{
+							type: "column-chart",
+							title: "Highest Education - All Adults",
+							value: {
+								fields: [
+									"B15002_calc_numLTHSE",
+									"B15002_calc_numHSE",
+									"B15002_calc_numSomeCollE",
+									"B15002_calc_numAAE",
+									"B15002_calc_numGEBAE",
+								],
+							},
+						},
+						{
+							type: "pie-chart",
+							title: "Highest Degree - Female",
+							value: {
+								fields: [
+									"B15002_028E",
+									"B15002_031E",
+									"B15002_032E",
+									"B15002_033E",
+									"B15002_034E",
+									"B15002_035E",
+								],
+							},
+						},
+						{
+							type: "pie-chart",
+							title: "Highest Degree - Male",
+							value: {
+								fields: [
+									"B15002_011E",
+									"B15002_014E",
+									"B15002_015E",
+									"B15002_016E",
+									"B15002_017E",
+									"B15002_018E",
+								],
+							},
+						},
+					],
+				},
+			],
+			fieldInfos: [
+				// Field formatting for total adult population.
+				{
+					fieldName: "B15002_calc_numLTHSE",
+					label: "No High School Degree",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_calc_numHSE",
+					label: "High School Degree (or equivalent)",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_calc_numSomeCollE",
+					label: "Some College",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_calc_numAAE",
+					label: "Associates Degree",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_calc_numGEBAE",
+					label: "Bachelor's Degree or Higher",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				// Field formatting for female fields.
+				{
+					fieldName: "B15002_028E",
+					label: "High School",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_031E",
+					label: "Associate's",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_032E",
+					label: "Bachelor's",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_033E",
+					label: "Master's",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_034E",
+					label: "Professional School (beyond Bachelor's)",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_035E",
+					label: "Doctorate",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				// Field formatting for male fields.
+				{
+					fieldName: "B15002_011E",
+					label: "High School",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_014E",
+					label: "Associate's",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_015E",
+					label: "Bachelor's",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_016E",
+					label: "Master's",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_017E",
+					label: "Professional School (beyond Bachelor's)",
+					format: {
+						digitSeparator: true,
+					},
+				},
+				{
+					fieldName: "B15002_018E",
+					label: "Doctorate",
+					format: {
+						digitSeparator: true,
+					},
+				},
 			],
 		},
 	});
 
-	const map = new Map({
-		basemap: "dark-gray-vector",
-		layers: [tractsLayer],
-	});
-
 	const view = new MapView({
 		container: viewDiv,
-		map: map,
+		map: new Map({
+			basemap: "dark-gray-vector",
+			layers: [tractsLayer],
+		}),
 		zoom: 9,
 		center: [-117.66256, 33.93037],
+		popup: {
+			dockEnabled: true,
+			dockOptions: {
+				position: "top-right",
+				breakpoint: false,
+			},
+		},
 	});
-
 	view.ui.add(new Legend({ view }), "bottom-left");
-
-	reactiveUtils.watch(
-		() => !view.updating,
-		() => {
-			console.log(view.zoom);
-			console.log(view.center);
-		}
-	);
 });
